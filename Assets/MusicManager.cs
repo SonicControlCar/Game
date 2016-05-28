93a9e04f0d08fee1437cf7fb5498e76fb7a67522
sam <android.ames@gmail.com>
2016-05-28 14:34:59 +1000

    inital project

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    void OnAwake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+	// Use this for initialization
+	void Start () {
+
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    void OnLevelWasLoaded(int level)
+    {
+        Debug.Log("playing clip: ");
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        Debug.Log("playing clip: " + thisLevelMusic.ToString());
+
+        if(thisLevelMusic)
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PanelFader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PanelFader : MonoBehaviour {
+
+    public float panelFadeInTime;
+
+    private Image fadePanel;
+    private Color currentColor = Color.black;
+
+
+	// Use this for initialization
+	void Start () {
+
+        fadePanel = GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.timeSinceLevelLoad < panelFadeInTime)
+        {
+            //fade in the panel
+            float alphaChange = Time.deltaTime / panelFadeInTime;
+            currentColor.a -= alphaChange;
+            fadePanel.color = currentColor;
+
+        }
+        else
+        {
+            gameObject.SetActive(false); //turn off the panel so that the buttons can be clicked, this removes the tick from the top of the screen option object
+        }
+	}
+}
